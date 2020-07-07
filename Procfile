dece4b30b88d76e7e6e4b466f4621450f4fbede6
mohammad salah ali <MsMainAcc0unt@gmail.com>
2020-07-07 15:01:53 +0200

    production v1.0

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: daphne Tolk.asgi:application --port $PORT --bind 0.0.0.0 -v2
+web: bin/start-pgbouncer-stunnel daphne Tolk.asgi:application --port $PORT --bind 0.0.0.0 -v2
--- Tolk/asgi.py
+++ Tolk/asgi.py
@@ -4,6 +4,6 @@ import django
 from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Tolk.settings")
-
+os.environ['ASGI_THREADS'] = "4"
 django.setup()
 application = get_default_application()
