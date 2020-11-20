f806d0a911b063dfc9f8a6df91bc4c42b69d2816
mohammad salah ali <MsMainAcc0unt@gmail.com>
2020-11-21 00:53:25 +0200

    change debug mode true

--- Tolk/settings.py
+++ Tolk/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "os.environ.get('SECRET_KEY')"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # hosts allowed for use with this application
 ALLOWED_HOSTS = [
