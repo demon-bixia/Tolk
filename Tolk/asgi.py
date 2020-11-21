eef5bca90b96efce8bd792f465434c96e62945cd
mohammad salah ali <MsMainAcc0unt@gmail.com>
2020-11-21 15:08:09 +0200

    fix secret key from "os.eviron.get()" to os.environ.get()

--- Tolk/asgi.py
+++ Tolk/asgi.py
@@ -4,6 +4,6 @@ import django
 from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Tolk.settings")
-os.environ['ASGI_THREADS'] = "19"
+os.environ['ASGI_THREADS'] = "13"
 django.setup()
 application = get_default_application()
